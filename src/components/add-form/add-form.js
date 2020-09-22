ba7983df7b20b723046bf2035b67dc367b14d93c
Bohdan Strukov <bodya19981997@gmail.com>
2020-09-22 11:13:48 +0300

    add style for form + posts'

--- src/components/add-form/add-form.js
+++ src/components/add-form/add-form.js
@@ -2,10 +2,14 @@ import React from "react";
 
 export const AddForm = () => {
     return (
-        <div className="form__add">
-            <div className="form__wrapper">My Posts</div>
-            <input type="text" className="form__input" placeholder="Enter your news..."/>
-            <button className="form__button">Send</button>
+
+        <div className="form">
+            <hr className="hr"/>
+            <div className="form__title">My Posts</div>
+            <div className="form__wrapper">
+                <input type="text" className="form__input" placeholder="Enter your news..."/>
+                <button className="form__button"><a className="" href="#">Send</a></button>
+            </div>
         </div>
     )
 }
--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -3,16 +3,14 @@ import React from "react";
 export const Posts = () => {
     return (
         <div className="posts">
-            <div className="posts__wrapper">
-                <div className="posts__items">
-                    <div className="post__item">
-                        <img src="https://www.pngkey.com/png/full/353-3534482_avatar-portfolio-02-avatar-sketch-cartoon-avatar.png" alt="avatar"/>
-                        <div className="post__title">Hey, why nobody love me?</div>
-                    </div>
-                    <div className="post__item">
-                        <img src="https://www.pngkey.com/png/full/353-3534482_avatar-portfolio-02-avatar-sketch-cartoon-avatar.png" alt="avatar"/>
-                        <div className="post__title">It`s our new program! Hey!</div>
-                    </div>
+            <div className="posts__items">
+                <div className="posts__item">
+                    <img src="https://www.pngkey.com/png/full/353-3534482_avatar-portfolio-02-avatar-sketch-cartoon-avatar.png" className="posts__img" alt="avatar"/>
+                    <div className="posts__title">Hey, why nobody love me?</div>
+                </div>
+                <div className="posts__item">
+                    <img src="https://www.pngkey.com/png/full/353-3534482_avatar-portfolio-02-avatar-sketch-cartoon-avatar.png" className="posts__img" alt="avatar"/>
+                    <div className="posts__title">It`s our new program! Hey!</div>
                 </div>
             </div>
         </div>
